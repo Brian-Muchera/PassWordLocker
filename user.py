f93eb485ef781545f08112c01ed26c3f40d715d8
Brian-Muchera <mucherabrian2@gmail.com>
2020-09-07 10:59:24 +0300

    create user class that generats instances for user

---
+++ user.py
@@ -0,0 +1,14 @@
+class User:
+
+    """
+    Create User class that gnerates new instances for uer.
+    """
+
+    user_list = []
+
+    def __init__(self, username, password):
+        """
+        Method taht defines the properties of a user
+        """
+        self.username = username
+        self.password = password
