98b0a87fbb37a105012d81ef2821f3334f74262d
Brian-Muchera <mucherabrian2@gmail.com>
2020-09-07 11:16:50 +0300

    added method that runs before each individualntest

--- user_test.py
+++ user_test.py
@@ -6,3 +6,10 @@ class TestUser(unittest.TestCase):
     """
     A Test class that defines test cases for the User class.
     """
+
+
+    def setUp(self):
+        """
+        Method that runs before each individual test methods run.
+        """
+        self.new_user = User('brian', 'muchera' 'steph1')
