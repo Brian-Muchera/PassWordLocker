3175e33881420008512808a77d0f936f1c9f3ca4
Brian-Muchera <mucherabrian2@gmail.com>
2020-09-07 11:02:33 +0300

    modified user.py

--- user.py
+++ user.py
@@ -8,7 +8,7 @@ class User:
 
     def __init__(self, username, password):
         """
-        Method taht defines the properties of a user
+        Method that defines the properties of a user
         """
         self.username = username
         self.password = password
