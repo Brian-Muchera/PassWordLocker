77304ca0c68d391ac317c1b2190306c31d8a48cd
Brian-Muchera <mucherabrian2@gmail.com>
2020-09-07 11:27:19 +0300

    added method to check if new user has been saved

--- user_test.py
+++ user_test.py
@@ -7,8 +7,7 @@ class TestUser(unittest.TestCase):
     A Test class that defines test cases for the User class.
     """
 
-
-    def setUp(self):
+def setUp(self):
         """
         Method that runs before each individual test methods run.
         """
@@ -20,3 +19,14 @@ class TestUser(unittest.TestCase):
         """
         self.assertEqual(self.new_user.username, 'brianmuchera')
         self.assertEqual(self.new_user.password, 'step1')
+
+ def test_save_user(self):
+        """
+        Check if a new user instance has been saved into the User list
+        """
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list), 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
