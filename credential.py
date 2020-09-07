6f8883777945b8b47a7cd2405f20a729bed6bf7d
Brian-Muchera <mucherabrian2@gmail.com>
2020-09-07 11:31:02 +0300

    added method to save and delete credentials in the list

--- credential.py
+++ credential.py
@@ -18,3 +18,27 @@ class Credentials():
         self.account = account
         self.userName = userName
         self.password = password
+
+       @classmethod
+    def verify_user(cls, username, password):
+        """
+        Method to verify if in the user_list
+        """
+        a_user = ""
+        for user in User.user_list:
+            if(user.username == username and user.password == password):
+                a_user == user.username
+        return a_user
+
+    def save_details(self):
+        """
+        Save new credentials in the credentials list
+        """
+        Credentials.credentials_list.append(self)
+
+    def delete_credentials(self):
+        """
+        Delete credentials in the credentilas list
+        """
+        Credentials.credentials_list.remove(self)
+  
